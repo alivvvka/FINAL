545849c192488295efc801138cbf038d4e1ad577
alivvvka <91064798+alivvvka@users.noreply.github.com>
2021-11-25 22:09:05 +0600

    Delete FINAL directory

--- FINAL/src/com/company/observer/NotifyManager.java
+++
@@ -1,9 +0,0 @@
-package com.company.observer;
-
-public interface NotifyManager {
-    void registerSub(Observer sub);
-
-    void removeSub(Observer sub);
-
-    void notifySubs();
-}
--- FINAL/src/com/company/observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.company.observer;
-
-import com.company.factory.Furniture;
-
-import java.util.List;
-
-public interface Observer {
-    void update(List<Furniture> furs);
-}
